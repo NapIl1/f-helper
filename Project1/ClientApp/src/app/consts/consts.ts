214edc9beac8c6d93b783df04baed92b224ebab7
NapIl1 <dencezar@gmail.com>
2024-06-29 22:49:19 +0300

    updated config for angular

--- Project1/ClientApp/src/app/consts/consts.ts
+++ Project1/ClientApp/src/app/consts/consts.ts
@@ -1,5 +1,5 @@
-//export const API_URL = 'https://litak-temp-ca.whitedesert-053b82c7.westeurope.azurecontainerapps.io/api/';
-export const API_URL = 'http://localhost:5109/';
+export const API_URL = 'https://fpv-training-location.ambitioussmoke-69ddee54.westeurope.azurecontainerapps.io/';
+//export const API_URL = 'http://localhost:5109/';
 
 export const ROLES = {
     INSTRUCTOR: 'instructor',
